63c73c70cfeec858a9c455b4e35abde8db5e6549
cpfarrell <farrell.chris1@gmail.com>
2013-09-02 19:47:45 -0700

    End of day

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def readcsvfile(file, CastEvals = [str, str, str, str, str]):
     lines = []
 
     with open(file, 'rb') as csvfile:
-        next(csvfile)
+        title = next(csvfile)
 
         reader = csv.reader(csvfile, delimiter=',')
 
@@ -51,7 +51,7 @@ def plotHist(Values, Xmin = 0, Xmax = 0, Ymin = 0, Ymax = 0, Bins = 100., histty
     ax = plt.subplot(111)
     bins = np.linspace(Xmin, Xmax, Bins)
 
-    plt.hist(np.clip(Values, Xmin, Xmax), bins, histtype=histtype, normed=normed, cumulative = cumulative)
+    plt.hist(np.clip(Values, Xmin, Xmax), bins, histtype = histtype, normed = normed, cumulative = cumulative)
 
     plt.xlim([Xmin, Xmax])
 
@@ -79,9 +79,11 @@ def determineSessionLength(accountVisits):
     deltat = np.array(deltat)
 
     plotHist(deltat, Xmin = 0, Xmax = 120, Bins = 120, normed = True, yscale = "log")
-    plotHist(deltat, Xmin = 0, Xmax = 120, Ymin = 0.7, Ymax = 1.0, Bins = 120, normed = True, yscale = "linear", cumulative = True)
+    plotHist(deltat, Xmin = 0, Xmax = 120, Ymin = 0.85, Ymax = 1.0, Bins = 120, normed = True, yscale = "linear", cumulative = True)
 
     plotHist(deltat, Xmin = 0, Xmax = 10000, Bins = 75, yscale = "log")
+
+
 
 def sessionProperties(accountSessions):
 
@@ -89,15 +91,33 @@ def sessionProperties(accountSessions):
     for acc in accountSessions:
         allSessions.extend(accountSessions[acc])
 
+    multipleVisits = np.zeros(len(allSessions))
 
-    sessionNodes = np.asarray([len(session) for session in allSessions])
-    sessionTime = np.asarray([deltaMinutes(session[len(session)-1]["time"], session[0]["time"]) for session in allSessions])
+    for i, session in enumerate(allSessions):
+        
+        pathsVisited = {}
+        totalNodes = 0
 
+        for action in session["actions"]:
+
+            if action["type"] == "node":
+
+                pathsVisited[action["path"]] = True
+                totalNodes += 1
+
+        #Difference 
+        multipleVisits[i] = totalNodes - len(pathsVisited)
+
+    sessionNodes = np.asarray([len(session["actions"]) for session in allSessions])
+    sessionTime = np.asarray([deltaMinutes(session["endTime"], session["startTime"]) for session in allSessions])
+
+    
     plotHist(sessionNodes, Xmin = 1, Xmax = 50, Bins = 50, normed = True, yscale = "log")
-
     plotHist(sessionTime, Xmin = 0, Xmax = 50, Bins = 50, normed = True, yscale = "log")
+    plotHist(np.divide(sessionTime, sessionNodes), Xmin = 0, Xmax = 6, Bins = 50, normed = False, yscale = "log")
+    plotHist(multipleVisits, Xmin = 0, Xmax = 50, Bins = 50, normed = True, yscale = "log")
 
-    plotHist(np.divide(sessionTime, sessionNodes), Xmin = 0, Xmax = 10, Bins = 50, normed = True, yscale = "log")
+
 
 def main():
 
@@ -114,12 +134,19 @@ def main():
         acc = visit[1]
 
         if acc in accountVisits:
-            accountVisits[acc].append({"time":visit[2], "path":visit[3]})
+            accountVisits[acc].append({"time":visit[2], "path":visit[3], "type":"node"})
         
         else:
-            accountVisits[acc] = [{"time":visit[2], "path":visit[3]}]
+            accountVisits[acc] = [{"time":visit[2], "path":visit[3], "type":"node"}]
 
-    determineSessionLength(accountVisits)
+
+    for submission in submissions:
+        
+        acc = submission[1]
+
+        accountVisits[acc].append({"time":submission[2], "path":submission[3], "type":"submission", "result":submission[4]})
+
+    #determineSessionLength(accountVisits)
 
     accountSessions = {}
 
@@ -127,25 +154,36 @@ def main():
         accountVisits[acc] = sorted(accountVisits[acc], key=lambda k: k['time'])
         accountSessions[acc] = []
 
-        Session = []
+        actions = []
 
         for idx, visit in enumerate(accountVisits[acc]):
             if idx == 0:
-                session = [visit]
+                actions = [visit]
 
             else:
                 #Time between visits in minutes
                 deltaTime = deltaMinutes(visit["time"], accountVisits[acc][idx-1]["time"])
 
                 #New session, defined as 20 minutes from above
-                if deltaTime > 20:
-                    accountSessions[acc].append(session)
-                    session = [visit]
+                if deltaTime > 15:
+                    accountSessions[acc].append({"actions":actions})
+                    actions = [visit]
 
                 else:
-                    session.append(visit)
+                    actions.append(visit)
 
-        accountSessions[acc].append(session)
+        accountSessions[acc].append({"actions":actions})
+
+
+
+
+    for account in accountSessions:
+        
+        sessions = accountSessions[account]
+
+        for session in sessions:
+            session["startTime"] = session["actions"] [0] ["time"]
+            session["endTime"] = session["actions"] [len(session["actions"]) -1] ["time"]
 
 
     sessionProperties(accountSessions)
